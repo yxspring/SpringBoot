223a9d01fdca2ce6c8967b904d7b7a926f412cd1
yxspring <3301068083@qq.com>
2020-02-28 17:31:48 +0800

    第六节课刚开始

---
+++ course06/src/test/java/com/itcodai/course06/Course06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcodai.course06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
