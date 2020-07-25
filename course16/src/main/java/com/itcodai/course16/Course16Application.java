806489eec3aa7918db0de4d7334fd98a7549d37d
ZYXS <37678999+yxspring@users.noreply.github.com>
2020-07-25 14:57:47 +0800

    Spring操作shiro

--- course16/src/main/java/com/itcodai/course16/Course16Application.java
+++ course16/src/main/java/com/itcodai/course16/Course16Application.java
@@ -1,9 +1,14 @@
 package com.itcodai.course16;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yuxingsheng
+ */
 @SpringBootApplication
+@MapperScan("com.itcodai.course16.dao")
 public class Course16Application {
 
 	public static void main(String[] args) {
